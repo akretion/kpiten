e4e748e80ae4d2de48997048ae6a657599e7596c
David Beal <david.beal@akretion.com>
2026-09-14 17:21:13 +0200

    ADD kpiten_demo module

---
+++ kpiten_demo/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "KPI Ten Demo Data",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "summary": "Demo data for KPI Ten dashboard",
+    "author": "Akretion",
+    "license": "LGPL-3",
+    "depends": [
+        "kpiten",
+        "erp_demo_generator",
+        "sale_management",
+        "purchase",
+        "purchase_stock",
+    ],
+    "data": [
+        "data/panels.xml",
+        "data/datasets.xml",
+        "data/tiles.xml",
+    ],
+    "installable": True,
+}
